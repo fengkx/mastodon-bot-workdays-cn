3674d81eb50f1d6b41b96470231e1a80b1168427
fengkx <liangkx8237@gmail.com>
2023-10-25 17:25:52 +0800

    wip

--- tests/test_holiday_data.py
+++ tests/test_holiday_data.py
@@ -18,3 +18,43 @@ async def test_holiday_data():
     assert len(holiday.all_days) == len(holiday.holidays) + len(holiday.workdays)
 
     assert len(holiday.all_days) == 365
+
+@pytest.mark.asyncio
+async def test_holiday_data_2024():
+    holiday = await HolidayDataByYear.create(2024)
+    assert len(holiday.all_days) == 366
+    
+    assert len(holiday.all_days) == len(holiday.holidays) + len(holiday.workdays)
+    weekend = (list(filter(lambda d: d.date.isoweekday() == 6 or d.date.isoweekday() == 7, holiday.all_days)))
+    print("len(weekend)",len(weekend))
+
+    working_weekend = []
+    for d in holiday.all_days:
+        isoweekday = d.date.isoweekday()
+        if (isoweekday == 6 or isoweekday == 7) and not d.is_off:
+            working_weekend.append(d)
+    assert len(working_weekend) == 8
+    
+    normal_weekend = list(filter(lambda d: d.data is None, weekend))
+    special_weekend = list(filter(lambda d: d.data is not None, weekend))
+    rest_weekend = list(filter(lambda d: d.is_off, weekend))
+    public_holiday = list(filter(lambda d: d.is_off and d.data is not None, holiday.all_days))
+    print('len normal_weekend', len(normal_weekend), 'len(special_weekend)',len(special_weekend), 'len(rest_weekend)', len(rest_weekend), 
+          'len(public_holiday)', len(public_holiday)
+          )
+    
+    
+    true_holidays = public_holiday + rest_weekend - working_weekend
+    wrong_holidays = (holiday.holidays)
+    # print(true_holidays.difference(wrong_holidays))
+    print('len(true_holidays)',len(true_holidays), 'len(wrong_holidays)',len(wrong_holidays))
+    true_holidays = set(true_holidays)
+    wrong_holidays = set(wrong_holidays)
+    print('len(true_holidays)',len(true_holidays), 'len(wrong_holidays)',len(wrong_holidays))
+
+    # assert len(holiday.holidays) == (len(public_holiday) + len(rest_weekend))
+
+    # # assert len(holiday.workdays) == 243
+
+    
+
